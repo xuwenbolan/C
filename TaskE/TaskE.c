804ca78e7cbccf7e84ca08f4539132c12fc5c60b
Chaganhu <xuwenbolan@163.com>
2022-12-13 22:35:12 +0800

    添加第4题

--- TaskE/TaskE.c
+++ TaskE/TaskE.c
@@ -2,6 +2,28 @@
 
 int main()
 {
-    printf("hello world!\n");
+    int a[3][4][5] = {0}, *p1 = a, **p2, (*p3)[5], *p4[4];
+    for (int i = 1; i <= 60; i++)
+        *(p1 + i - 1) = i;
+
+    for (int i = 0; i < 3; i++)
+    {
+        p1 = a[i];
+        p2 = p4;
+        p3 = a[i];
+        p4[0] = p1;
+        p4[1] = p1 + 5;
+        p4[2] = p1 + 10;
+        p4[3] = p1 + 15;
+        for (int k = 0; k < 4; k++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                printf("%-3d%-3d%-3d%-3d", *(p1++), p2[k][j], p3[k][j], p4[k][j]);
+                printf("\n");
+            }
+        }
+    }
+
     return 0;
 }
