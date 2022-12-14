86daac5bdaf3175b4247e8ada8ec07744c16e1cb
Chaganhu <xuwenbolan@163.com>
2022-12-14 11:11:20 +0800

    增加第6题

--- TaskE/test.c
+++ TaskE/test.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct string
+{
+    char *str;
+    struct string *next;
+} LinkList;
+
 int main()
 {
-    int a[4][5] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    char temp[50]={0};
+    LinkList *head, *node, *end;
+	head = (LinkList*)malloc(sizeof(LinkList));
+	end = head;
+	while(1)
+    {
+		node = (LinkList*)malloc(sizeof(LinkList));
+        gets(temp);
+        if(temp[0]==0)break;
+        node->str=(char*)malloc(strlen(temp)+1);
+        strcpy(node->str,temp);
+		end->next = node;
+		end = node;
+	}
+	end->next = NULL;
+
+    while (head->next != NULL) 
+    {
+		head = head->next;
+		printf("%s", head->str);
+	}
+}
+
+
+/* T4
+int a[4][5] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     int *p1;
     int **p2;
     int(*p3)[5];
     int *p4[4];
-    
+
     p1 = a[0];
     p2 = p4;
     p3 = a;
@@ -23,4 +57,4 @@ int main()
             printf("\n");
         }
     }
-}
+*/
